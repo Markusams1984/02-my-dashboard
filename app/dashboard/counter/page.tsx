b2b3e6eab854bd430462c6b09f94ad2c0e56bca9
Marco-Andres Munoz <marco.a.m.s.84@gmail.com>
2026-09-05 18:19:54 -0400

    CartCounter con valor inicial desde props

--- app/dashboard/counter/page.tsx
+++ app/dashboard/counter/page.tsx
@@ -1,8 +1,19 @@
+import { CartCounter } from "@/app/shopping-cart/components/CartCounter";
+
+export const metadata = {
+  title: 'Shopping Cart',
+  description: 'This is the shopping cart page',
+};
 
 export default function CounterPage() {
+  
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Productos en el carrito</span>
+      <CartCounter value={ 20 }/>
     </div>
   );
 }
+
+
+
