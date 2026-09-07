8c91576a858cc1ecf1344f4de8524310359f9964
Marco-Andres Munoz <marco.a.m.s.84@gmail.com>
2026-09-07 19:36:11 -0300

    Sección 5: componentes PokemonGrid y PokemonCard

--- app/dashboard/pokemons/page.tsx
+++ app/dashboard/pokemons/page.tsx
@@ -1,5 +1,4 @@
-import { SimplePokemon, PokemonsResponse} from "@/app/pokemons";
-import Image from "next/image";
+import { SimplePokemon, PokemonsResponse, PokemonGrid} from "@/app/pokemons";
 
 const getPokemons = async ( limit= 20, offset= 0 ): Promise<SimplePokemon[]> => {
     const data: PokemonsResponse = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
@@ -21,17 +20,11 @@ export default async function PokemonsPage() {
   
     return (
     <div className="flex flex-col">
-      <div className="flex flex-wrap gap-10 items-center justify-center">
-        {pokemons.map(pokemon => (
-        <Image
-        key={pokemon.id}
-        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-        width={100}
-        height={100}
-        alt={pokemon.name}
-  />
-))}
-      </div>
+    
+    <span className="text-5xl my-2">Listado de Pokemons <small>estatico</small></span>
+
+        <PokemonGrid pokemons={pokemons} />
+
     </div>
   );
 }
