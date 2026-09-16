316acfec89d6f705839acbc04a7620ee8a2f5ebb
Marco-Andres Munoz <marco.a.m.s.84@gmail.com>
2026-09-16 18:02:37 -0300

    feat: página de detalle de pokemon /dashboard/pokemon/[id]

--- src/pokemons/components/PokemonCard.tsx
+++ src/pokemons/components/PokemonCard.tsx
@@ -13,8 +13,8 @@ export const PokemonCard = ({ pokemon }: Props) => {
     
     return (
         <div className="mx-auto right-0 mt-2 w-60">
-            <div className="flex flex-col bg-white rounded overflow-hidden shadow-lg">
-                <div className="flex flex-col items-center justify-centertext-center p-6 bg-gray-800 border-b">
+            <div className="flex flex-col bg-white rounded overflow-hidden shadow-lg h-full">
+                <div className="flex flex-col items-center justify-center text-center p-6 bg-gray-800 border-b">
                         <Image
                         key={pokemon.id}
                         src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
@@ -22,12 +22,13 @@ export const PokemonCard = ({ pokemon }: Props) => {
                         height={100}
                         alt={pokemon.name}
                         priority={ false }
+                        className="h-25 w-25 object-contain"
                         />
                     
                     <p className="text-base font-bold text-gray-100 capitalize">{ name }</p>
                     <div className="mt-5">
                         <Link
-                            href={`dashboard/pokemon/${id}`}
+                            href={`/dashboard/pokemon/${id}`}
                             className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
                         >
                             Mas informacion
